62e4e812c7fde9173f31d19dc02125bea561ecba
aohongbo <aohongbo@kuaishou.com>
2018-01-31 19:57:18 +0800

    add example code

---
+++ src/chapter1/fact.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+template<int N>
+class Fact {
+    public:
+        enum{value = N * Fact<N-1>::value};
+};
+
+template<>
+class Fact<0> {
+    public:
+        enum{value=1};
+};
+
+
+
+int main(int argc, char* argv[]) {
+    cout<<Fact<10>::value<<endl;
+}
+
+

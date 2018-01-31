167083ea35775fc4d9db9132eecfb371b9f8a047
aohongbo <aohongbo@kuaishou.com>
2018-01-31 23:34:13 +0800

    add code

---
+++ src/include/common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_HEADER__
+#define __COMMON_HEADER__
+#include<iostream> 
+using namespace std;
+
+
+#define BEGIN_MAIN int main(int argc, char* argv[]) {
+
+
+#define END_MAIN return -1; }
+
+#endif
